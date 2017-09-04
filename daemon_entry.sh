07d318ce824fa5de1667e02247f7c851075a3e86
nemd <nemrod@reaper.pl>
2017-09-04 20:16:55 +0200

    final changes for remote auth

--- daemon_entry.sh
+++ daemon_entry.sh
@@ -6,10 +6,8 @@ deluged -c /config && sleep 3 && killall deluged
 echo "create initial user"
 echo "$deluser:$delpass:$lvl" >> /config/auth
 
+echo "enable remote access"
+sed -i '/allow_remote/ s/false/true/' /config/core.conf
+
 echo "start daemon"
 deluged -d -c /config
-
-echo "enable reamote access"
-deluge-console "config -s allow_remote True"
-deluge-console "config allow_remote"
-
