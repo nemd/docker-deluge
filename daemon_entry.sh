f85bb1776bf0bf4f7a3c2073022a52da1a6c4ad0
nemd <nemrod@reaper.pl>
2017-09-04 19:58:55 +0200

    fixes for entry

--- daemon_entry.sh
+++ daemon_entry.sh
@@ -7,9 +7,10 @@ killall deluged
 echo "create initial user"
 echo "$deluser:$delpass:$lvl" >> /config/auth
 
+echo "start daemon"
+deluged -d -c /config
+
 echo "enable reamote access"
 deluge-console "config -s allow_remote True"
 deluge-console "config allow_remote"
 
-echo "start daemon"
-deluged -d -c /config
